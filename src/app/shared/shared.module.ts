68742750baf9c024d899572af0e52f2c42bb960c
juapasan <juapasan@bancolombia.com.co>
2024-09-28 11:36:04 -0500

    Se construyen las paginas con su buscador

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { RouterModule } from '@angular/router';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
+    SearchBoxComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
+    SearchBoxComponent
   ]
 })
 export class SharedModule { }
